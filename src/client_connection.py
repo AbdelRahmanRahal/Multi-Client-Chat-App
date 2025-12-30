5bd594a6b0d8525e1489a4a6df9217c9ad1c3b16
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2025-12-30 10:48:51 +0200

    feat: added the ability to talk on LAN

--- src/client_connection.py
+++ src/client_connection.py
@@ -3,11 +3,11 @@ import ssl
 import sys
 
 if len(sys.argv) < 2:
-    print("Usage: python client.py <username>")
+    print("Usage: python client.py <username> [server_ip]")
     sys.exit(1)
 
 USERNAME = sys.argv[1]
-SERVER_IP = "127.0.0.1"
+SERVER_IP = sys.argv[2] if len(sys.argv) > 2 else "127.0.0.1"
 PORT = 5000
 
 sock = socket.socket()
